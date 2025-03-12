5c442115db8928a962cefae98507ef1b34fbcf24
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-03-12 14:59:10 -0600

    all components are in folders

---
+++ src/components/ActivityDisplayed/ActivityCard.tsx
@@ -0,0 +1,23 @@
+interface ActivityCardProps {
+  imageUrl?: string;
+  title: string;
+}
+
+export const ActivityCard = ({ imageUrl, title }: ActivityCardProps) => {
+  return (
+    <article className="flex flex-col items-center">
+      {imageUrl ? (
+        <img
+          src={imageUrl}
+          alt={title}
+          className="w-[150px] h-[150px] border border-black shadow-[0px_4px_4px_3px_rgba(0,_0,_0,_0.25)]"
+        />
+      ) : (
+        <div className="border border-black shadow-sm h-[150px] w-[150px]" />
+      )}
+      <h3 className="mt-3.5 text-lg leading-6 text-center text-black font-bold">
+        {title}
+      </h3>
+    </article>
+  );
+};
---
+++ src/components/ActivityDisplayed/ActivityGrid.tsx
@@ -0,0 +1,53 @@
+import { ActivityCard } from "./ActivityDisplayed/ActivityCard";
+import { DropdownIcon } from "./Home Page/Icons";
+
+export const ActivityGrid = () => {
+  return (
+    <section className="flex flex-col items-center px-8 mt-8">
+      <button className="flex justify-center items-center mb-10 bg-cyan-600 border border-black shadow-sm h-[43px] w-[254px]">
+        <span className="text-lg leading-6 text-center text-black">
+          Physical
+        </span>
+        <div className="flex justify-center items-center w-14 h-12">
+          <DropdownIcon />
+        </div>
+      </button>
+
+      <div className="grid grid-cols-2 gap-10">
+        <ActivityCard
+          imageUrl="https://cdn.builder.io/api/v1/image/assets/TEMP/f2aebd9b903b54407cc2c26ae7a282057725d5cb"
+          title="Soccer"
+        />
+        <ActivityCard
+          imageUrl="https://cdn.builder.io/api/v1/image/assets/TEMP/60fcbe910dd3c06118425cfd5388515378d98c65"
+          title="Board Games"
+        />
+      </div>
+
+      <div className="grid grid-cols-2 gap-10 mt-10">
+        <ActivityCard title="Description" />
+        <ActivityCard title="Description" />
+      </div>
+
+      <div className="grid grid-cols-2 gap-10 mt-10">
+        <ActivityCard title="Description" />
+        <ActivityCard title="Description" />
+      </div>
+
+      <div className="grid grid-cols-2 gap-10 mt-10">
+        <ActivityCard title="Description" />
+        <ActivityCard title="Description" />
+      </div>
+
+      <div className="grid grid-cols-2 gap-10 mt-10">
+        <ActivityCard title="Description" />
+        <ActivityCard title="Description" />
+      </div>
+
+      <div className="grid grid-cols-2 gap-10 mt-10">
+        <ActivityCard title="Description" />
+        <ActivityCard title="Description" />
+      </div>
+    </section>
+  );
+};
