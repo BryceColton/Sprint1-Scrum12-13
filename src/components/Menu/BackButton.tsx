cef234ed2ee4fbf87be7a8bc2726d16d68a82269
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-03-19 13:12:16 -0600

    menu page

---
+++ src/components/Menu/BackButton.tsx
@@ -0,0 +1,22 @@
+export const BackButton: React.FC = () => {
+    return (
+      <button className="absolute top-4 left-5" aria-label="Go back">
+        <svg
+          width="67"
+          height="67"
+          viewBox="0 0 67 67"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M53.0416 33.5H13.9583M13.9583 33.5L33.4999 53.0417M13.9583 33.5L33.4999 13.9583"
+            stroke="#212121"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
+    );
+  };
+  
