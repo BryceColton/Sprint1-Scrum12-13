819d18bad2716c214613606e8d7ae86ac7735ba9
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-03-19 14:41:04 -0600

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Link } from "react-router-dom";
 import './App.css'
 import { YouthActivities } from './components/Login/YouthActivities'
 import { HomePage } from './components/HomePage/HomePage'
 import { CalendarView } from './components/Calendar/CalendarView'
 import { ActivityBrowser } from './components/ActivityDisplayed/ActivityBrowser'
 import { NavigationMenu } from './components/Menu/NavigationMenu'
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
 
 function App() {
@@ -14,7 +17,15 @@ function App() {
   return (
     <>
       
-      <NavigationMenu /> 
+      <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path='/HomePage' element={<HomePage />} />
+        <Route path="/login" element={<YouthActivities />} />
+        <Route path="/calendar" element={<CalendarView />} />
+        <Route path="/activities" element={<ActivityBrowser />} />
+        <Route path="/blog" element={<h1>Blog Page</h1>} />
+      </Route>
+    </Routes>
     </>
   )
 }
