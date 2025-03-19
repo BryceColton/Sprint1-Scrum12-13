821a000a466a970087e5e12831df591c9735d358
ZacharyKennedyBYU <153124492+ZacharyKennedyBYU@users.noreply.github.com>
2025-03-19 14:36:47 -0600

    Delete src directory

--- src/components/ActivityDisplayed/BottomIndicator.tsx
+++
@@ -1,7 +0,0 @@
-export const BottomIndicator = () => {
-  return (
-    <div className="flex justify-center items-center px-32 pt-5 pb-2 mt-auto w-full h-[34px]">
-      <div className="w-36 bg-black h-[5px] rounded-[100px]" />
-    </div>
-  );
-};
--- src/components/Calendar/BottomSheetIndicator.tsx
+++
@@ -1,7 +0,0 @@
-export function BottomSheetIndicator() {
-  return (
-    <footer className="flex justify-center items-center mt-auto h-[34px]">
-      <div className="w-36 bg-black h-[5px] rounded-[100px]" />
-    </footer>
-  );
-}
